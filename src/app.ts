63393d52f037a79cc3bb6d0b70cc4b4760f99516
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2023-01-23 13:42:01 +0100

    Working

--- src/app.ts
+++ src/app.ts
@@ -20,11 +20,17 @@ const list = new ListTemplate(ul)
 
 form.addEventListener("submit", (e: Event)=>{
     e.preventDefault();
+
+    let values:[string, string, number];
+    values = [tofrom.value, details.value, amount.valueAsNumber]
+
+
+
     let doc: HasFormatter;
     if (type.value === "invoice"){
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     } else{
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Payment(...values)
     }
     // console.log(doc)
     list.render(doc, type.value, "end")
